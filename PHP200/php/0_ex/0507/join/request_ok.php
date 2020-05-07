4ba2c8ad62240fbaab9aabacd507e9b7676be085
chaemilee <chaemilee9012@gmail.com>
2020-05-07 16:20:36 +0900

    0507

--- PHP200/php/0_ex/0507/join/request_ok.php
+++ PHP200/php/0_ex/0507/join/request_ok.php
@@ -4,18 +4,20 @@
   $pw = $_GET['pw'];
   $gender = $_GET['gender'];
   $city = $_GET['city'];
-  $how = $_GET['how'];
+  $how = implode('/', $_GET['how']);
+  // implode(구분 단위, 배열): 배열값을 문자열로 반환
   $content = $_GET['content'];
 
   echo $id."<br>";
   echo $pw."<br>";
   echo $gender."<br>";
   echo $city."<br>";
-  echo $how[0]."<br>";
-  echo $content;
+  echo $how."<br>";
+  echo nl2br($content);
+  // nl2br(): 문자열 줄바꿈 위치에 <br> 삽입
 
-  foreach($how as $value) {
-    $h .= $value."/";
-    echo "<br>".$h;
-  }
+  // foreach($how as $value) {
+  //   $h .= $value."/";
+  //   echo "<br>".$h;
+  // }
 ?>
